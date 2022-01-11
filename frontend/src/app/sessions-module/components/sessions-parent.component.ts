81821ddf3ed34ed9c7800e1bca2b6f0c9f6746f5
Sean A. Young <cname87@gmail.com>
2022-01-11 21:02:00 +0000

    Finalize sessions store

--- frontend/src/app/sessions-module/components/sessions-parent.component.ts
+++ frontend/src/app/sessions-module/components/sessions-parent.component.ts
@@ -3,19 +3,31 @@ import { ActivatedRoute, Data, ParamMap } from '@angular/router';
 import { NGXLogger } from 'ngx-logger';
 import { catchError, map, takeUntil } from 'rxjs/operators';
 import { Observable, Subject } from 'rxjs';
+import { IsLoadingService } from '@service-work/is-loading';
 
 import { UserIdStateService } from '../../app-module/services/user-id-state-service/user-id-state.service';
 import { ISession, ISessions } from '../models/sessions-models';
 import { SessionsStore } from '../store/sessions.store';
 
 /**
- * @title Parent component of the sessions table and the specific session editing form.
+ * @title Training sessions parent component.
  *
- * On startup it receives a sessions object from the route resolver and causes the sessions table component to be shown.
+ * There are two simple presentational components:
+ * - sessions table
+ * - session update form
  *
- * The sessions component will fire an event when a row in the sessions table is clicked, passing in sessions data and row data. The sessions data is saved for later updating, This component then displays the session update form, displaying the passed in session detail.
+ * There is a store which stores and manipulates all state.
  *
- * When the session form is submitted by the user the updated session detail is passed back to this component via an event. This component updates the previously stored sessions object with the updated session, updates the backend with the sessions object, and calls the sessions table component with the updated sessions object.
+ * This component handles all logic.
+ *
+ * Inputs:
+ *
+ * 1. On startup it receives a sessions object from the route resolver
+ * Note: This is passed to a child component which causes the sessions table component to be shown.
+ *
+ * The sessions component will fire an event when a row in the sessions table is clicked, passing in row data. This component then uses the store to get the corresponding session detail, and then displays the session update form, displaying the passed in session detail.
+ *
+ * When the session form is submitted by the user the updated session detail is passed back to this component via an event. This component updates the store sessions object with the updated session, updates the backend with the sessions object, and displays the sessions table component with the updated sessions object.
  */
 @Component({
   selector: 'app-session-parent',
@@ -24,8 +36,6 @@ import { SessionsStore } from '../store/sessions.store';
 })
 export class SessionsParentComponent implements OnInit, OnDestroy {
   //
-  /* member id passed to sessions component */
-  memberId!: number;
   /* show session or sessions components */
   showSession = false;
   showSessions = false;
@@ -39,6 +49,7 @@ export class SessionsParentComponent implements OnInit, OnDestroy {
   constructor(
     private route: ActivatedRoute,
     private userIdStateService: UserIdStateService,
+    private isLoadingService: IsLoadingService,
     private store: SessionsStore,
     private logger: NGXLogger,
   ) {
@@ -61,17 +72,20 @@ export class SessionsParentComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.logger.trace(`${SessionsParentComponent.name}: Starting ngOnInit`);
 
-    /* loads the sessions object from the route data to the sessions store and pass to the sessions$ observable */
+    /* loads the sessions object from the route data */
     const inputSessions$ = this.route.data.pipe(
       map((data: Data): ISessions => {
         return data.sessions;
       }),
     );
+    /* pass to the sessions store */
     this.store.loadSessions(inputSessions$);
+    /* pass an observable to the template */
     this.sessions$ = this.store.sessions$;
+    /* show the sessions child component */
     this.showSessions = true;
 
-    /* get member id from route state */
+    /* get member id from route state and pass it to user id service */
     this.route.paramMap
       .pipe(
         map((paramMap: ParamMap) => {
@@ -79,7 +93,6 @@ export class SessionsParentComponent implements OnInit, OnDestroy {
           if (!id) {
             throw new Error('id path parameter was invalid');
           }
-          this.memberId = +id;
           return id;
         }),
         takeUntil(this.#destroy$),
@@ -89,39 +102,53 @@ export class SessionsParentComponent implements OnInit, OnDestroy {
   }
 
   /**
-   * Called when a clicked row is passed from the sessions component. Causes the session edit page to be shown.
+   * Called when a clicked row is passed from the sessions component. Causes the session edit form to be shown.
+   * @param rowIndex$ The zero-based index of the clicked row.
    */
   editSession(rowIndex$: Observable<number>): void {
     this.logger.trace(`${SessionsParentComponent.name}: Starting editSession`);
 
+    /* exit if the table is loading due get sessions being called following a date change */
+    if (this.isLoadingService.isLoading({ key: `${SessionsStore.name}#1` })) {
+      return;
+    }
+
     /* store the clicked row in the sessions store */
     this.store.loadRowIndex(rowIndex$);
 
-    /* get the session from the input sessions object to allow it be sent to the session update page for updating */
-    this.session$ = this.store.session$ as Observable<ISession>;
+    /* get the session corresponding click from the store to be sent to the session update page */
+    this.session$ = this.store.session$;
 
     /* change the view to show the session update form */
     this.showSessions = false;
     this.showSession = true;
   }
 
+  /**
+   * @summary Takes a date from the sessions child component and calls a method to update the displayed sessions table.
+   * @param date The date passed in.
+   */
   newDate(date: Date) {
     this.store.getOrCreateSessions(date);
   }
+
   /**
-   * Called when an updated session, or undefined, is passed from the session update component.
+   * @summary Called when an updated session, or undefined, is passed from the session update component.
    * It sets the sessions$ property and causes an updated sessions table to be shown.
+   * @param newSessionOrUndefined The updated session, or undefined if the update was cancelled.
    */
   doneSession(newSessionOrUndefined: ISession | undefined): void {
     this.logger.trace(`${SessionsParentComponent.name}: Starting doneSession`);
+
     if (!!newSessionOrUndefined) {
       /* get an updated sessions object */
       this.store.updateSessionAtRowIndex(newSessionOrUndefined);
+      /* update the backend */
       this.store.updateSessions();
-      this.sessions$ = this.store.sessions$;
     } else {
-      /* if no session is passed in then return the unchanged sessions object back to the sessions page */
+      /* if no session is passed in then make no changes */
     }
+
     /* change the view */
     this.showSessions = true;
     this.showSession = false;
--- frontend/src/app/sessions-module/store/sessions.store.ts
+++ frontend/src/app/sessions-module/store/sessions.store.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ComponentStore, tapResponse } from '@ngrx/component-store';
 import { NGXLogger } from 'ngx-logger';
 import clonedeep from 'lodash.clonedeep';
+import { IsLoadingService } from '@service-work/is-loading';
 
 import { UserIdStateService } from '../../app-module/services/user-id-state-service/user-id-state.service';
 import { SessionsService } from '../services/sessions.service';
@@ -13,12 +14,12 @@ export interface ISessionsState {
   /* the training sessions object*/
   sessions: ISessions;
   /* the row on the sessions table that was last clicked */
-  rowIndex: number | undefined;
+  rowIndex: number;
 }
 
 const defaultState: ISessionsState = {
   sessions: blankSessions as ISessions,
-  rowIndex: undefined,
+  rowIndex: 0,
 };
 
 @Injectable()
@@ -27,39 +28,40 @@ export class SessionsStore extends ComponentStore<ISessionsState> {
   constructor(
     private sessionsService: SessionsService,
     private userId: UserIdStateService,
+    private isLoadingService: IsLoadingService,
     private logger: NGXLogger,
   ) {
+    /* load the default state */
     super(defaultState);
     this.logger.trace(`${SessionsStore.name}: Starting SessionsStore`);
   }
 
   /* ******** selectors ******** */
 
+  /* the training sessions */
   readonly sessions$ = this.select(({ sessions }) => {
     this.logger.trace(`${SessionsStore.name}: Starting sessions$`);
     return sessions;
   });
 
+  /* the training session corresponding to a row index */
   readonly session$ = this.select(({ sessions, rowIndex }) => {
     this.logger.trace(`${SessionsStore.name}: Starting session$`);
-    if (rowIndex !== undefined) {
-      return {
-        type: sessions.sessions[rowIndex].type,
-        rpe: sessions.sessions[rowIndex].rpe,
-        duration: sessions.sessions[rowIndex].duration,
-        comment: sessions.sessions[rowIndex].comment,
-      };
-    } else {
-      return undefined;
-    }
+    return {
+      type: sessions.sessions[rowIndex].type,
+      rpe: sessions.sessions[rowIndex].rpe,
+      duration: sessions.sessions[rowIndex].duration,
+      comment: sessions.sessions[rowIndex].comment,
+    };
   });
 
+  /* the clicked row index */
   readonly rowIndex$ = this.select(({ rowIndex }) => {
     this.logger.trace(`${SessionsStore.name}: Starting rowIndex$`);
     return rowIndex;
   });
 
-  /* ViewModel for the component */
+  /* view model for the component */
   readonly vm$ = this.select(
     this.sessions$,
     this.rowIndex$,
@@ -85,14 +87,12 @@ export class SessionsStore extends ComponentStore<ISessionsState> {
     return newState;
   });
 
+  /* takes a set of session data updates a training session corresponding to the stored row index */
   readonly updateSessionAtRowIndex = this.updater(
-    (state, session: ISession | undefined) => {
+    (state, session: ISession) => {
       this.logger.trace(
         `${SessionsStore.name}: Starting updateSessionAtRowIndex`,
       );
-      if (!session) {
-        return state;
-      }
       const rowIndex = state.rowIndex;
       const newState = clonedeep(state);
       if (rowIndex !== undefined && newState.sessions) {
@@ -107,13 +107,16 @@ export class SessionsStore extends ComponentStore<ISessionsState> {
 
   /* ******** effects ******** */
 
+  /* gets a sessions table from the backend corresponding to a date */
   readonly getOrCreateSessions = this.effect((date$: Observable<Date>) => {
+    const loadingOpt = { key: `${SessionsStore.name}#1` };
     return date$.pipe(
       /* use switchMap so a new call made if date changes and the previous one abandoned */
       switchMap((date) => {
         this.logger.trace(
           `${SessionsStore.name}: Starting getOrCreateSessions`,
         );
+        this.isLoadingService.add(loadingOpt);
         return this.sessionsService
           .getOrCreateSessions(this.userId.id, date)
           .pipe(
@@ -125,6 +128,7 @@ export class SessionsStore extends ComponentStore<ISessionsState> {
                     sessions,
                   )}`,
                 );
+                this.isLoadingService.remove(loadingOpt);
                 return this.loadSessions(sessions);
               },
               (e) => console.error(e),
@@ -134,9 +138,10 @@ export class SessionsStore extends ComponentStore<ISessionsState> {
     );
   });
 
+  /* passes the store sessions object to the backend to the backend */
   readonly updateSessions = this.effect((dummy$) => {
     return dummy$.pipe(
-      /* use concatMap calls will be completed in order */
+      /* use concatMap so calls will be completed in order */
       concatMap(() => {
         this.logger.trace(`${SessionsStore.name}: Starting updateSessions`);
         const sessions = this.get().sessions;
